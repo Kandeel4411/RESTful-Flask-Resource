11ac7d0c07aaad21889b7ec78a267d19c9701695
abdo <puppet4411@gmail.com>
2019-05-18 22:45:31 +0200

    Removed basic samples

--- app.py
+++ app.py
@@ -10,17 +10,7 @@ from flask import (Flask, abort, jsonify, make_response, redirect,
 
 app = Flask(__name__)
 
-campaigns = [
-    {"name": "n1",   "country": "USA",   "budget": 149,
-     "goal": "Awareness",   "category": "Technology"},
-    {"name": "n2",   "country": "USA",   "budget": 149,
-     "goal": "Awareness",   "category": "Sports"},
-    {"name": "n3",   "country": "EGY",   "budget": 149,
-     "goal": "Awareness",   "category": "Technology"},
-    {"name": "n4",   "country": "USA",   "budget": 149,
-     "goal": "Awareness",   "category": "Sports"},
-    {"name": "n5", "country": "EGY",   "budget": 149,
-     "goal": "Conversion",   "category": "Sports"}]
+campaigns = []
 
 
 @app.route("/api/campaigns", methods=["POST", "GET"])
